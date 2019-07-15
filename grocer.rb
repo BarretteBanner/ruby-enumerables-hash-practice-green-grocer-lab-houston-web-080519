75ad705eca1bb680d4890a99c3c0f32f1eacc9d5
Barrette Banner <barrettebanner@gmail.com>
2019-07-15 18:56:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,47 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+ new_cart = {}
+ cart.each do |items_array|
+   items_array.each do |item, attribute_hash|
+     new_cart[item] ||= attribute_hash
+     new_cart[item][:count] ? new_cart[item][:count] += 1 : new_cart[item][:count] = 1 
+   end
+ end
+ new_cart
 end
 
+
+  
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    coupon.each do |attribute, value|
+      name = coupon[:item]
+      if cart[name] && cart[name][:count] >= coupon[:num]
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] +=1 
+        else
+          binding.pry 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
+        end
+      cart[name][:count] -= coupon[:num]
+      end
+  end
 end
+cart 
+end
+
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
 end
 
 def checkout(cart, coupons)
